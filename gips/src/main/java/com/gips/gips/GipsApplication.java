4d9cdb6d9b578bdeed14947718868cf1d09c4c7b
Marco Antônio Meira de Lima <88397083+Marcokbc@users.noreply.github.com>
2023-02-20 15:51:38 -0300

    initializr

---
+++ gips/src/main/java/com/gips/gips/GipsApplication.java
@@ -0,0 +1,13 @@
+package com.gips.gips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GipsApplication.class, args);
+	}
+
+}
